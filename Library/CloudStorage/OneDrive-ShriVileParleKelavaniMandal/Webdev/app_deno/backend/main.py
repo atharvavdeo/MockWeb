0d849c32db6d4cf5be6572c76fe261b0a6fb4ec0
atharvavdeo <atharva.deo03@svkmmumbai.onmicrosoft.com>
2025-09-30 01:44:30 +0530

    Deployed

--- Library/CloudStorage/OneDrive-ShriVileParleKelavaniMandal/Webdev/app_deno/backend/main.py
+++ Library/CloudStorage/OneDrive-ShriVileParleKelavaniMandal/Webdev/app_deno/backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Configure CORS
 origins = [
     "http://localhost:5173",  # Your React app's default address
-    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5173","https://mock-web-theta.vercel.app",
 ]
 app.add_middleware(
     CORSMiddleware,
